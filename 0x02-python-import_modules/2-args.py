e25164d6c09189f0a6832c2e1cc9b48ee13e1ac4
Prila1 <yarteypriscy@gmail.com>
2023-09-08 08:41:11 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 2-args.py
+
+import sys
+if __name__ == "__main__":
+
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("0 arguments.")
+    elif args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(args))
+    for i in range(args):
+        print("{0}: {1}".format(i + 1, sys.argv[i + 1]))
