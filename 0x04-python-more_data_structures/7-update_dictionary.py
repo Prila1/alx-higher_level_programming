2a44c40925a8abb041aed711634b02ced3bff3f9
Prila1 <yarteypriscy@gmail.com>
2023-09-14 08:36:00 +0000

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dict, key, value):
+    if key in a_dict.keys():
+        a_dict[key] = value
+    else:
+        a_dict[key] = value
+    return a_dict
