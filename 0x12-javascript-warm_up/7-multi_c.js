0509e6c9b2be62955d72336177c22e091673e4b0
Prila1 <yarteypriscy@gmail.com>
2023-12-11 22:07:16 +0000

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+const num = Number(argv[2]);
+
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
