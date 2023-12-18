cac8ff4a6bfc68e1912d60598a7c564fcee2a352
Prila1 <yarteypriscy@gmail.com>
2023-12-18 08:05:57 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """
-    Displays all values in the states table of
+ Displays all values in the states table of
     hbtn_0e_0_usa where name matches the argument
 """
 
-
 import MySQLdb
-from sys import argv
-
+import sys
 
 if __name__ == "__main__":
+    args = sys.argv[1:]
+    mysql_user, mysql_pass, db_name, search_str = args
     db = MySQLdb.connect(
         host="localhost",
-        port=3306, user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
-    cur = db.cursor()
-    cur.execute("""SELECT id, name FROM states WHERE
-         name COLLATE latin1_general_cs LIKE
-         '{}%' ORDER BY id ASC;""".format(argv[4]))
-    rows = cur.fetchall()
+        user=mysql_user,
+        password=mysql_pass,
+        port=3306,
+        database=db_name,
+    )
+    QUERY = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(
+        search_str)
+
+    db.query(QUERY)
+
+    rows = db.store_result().fetch_row(maxrows=0)
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
