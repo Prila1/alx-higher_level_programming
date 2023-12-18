be594745865b0043f5a4ec8a749b66b532640bf4
Prila1 <yarteypriscy@gmail.com>
2023-12-18 08:42:34 +0000

    13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module perform some actions on the sql database using sqlalchemy
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    mysql_user, mysql_pass, db_name = args
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            mysql_user,
+            mysql_pass,
+            db_name,
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+
+    session.query(State).filter(State.name.contains('a')).delete()
+    session.commit()
+    session.close()
