f48a4c80913413e44c4d536a3888ba42b8bf922d
Prila1 <yarteypriscy@gmail.com>
2023-12-18 08:38:24 +0000

    11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module perform some actions on the sql database using sqlalchemy
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    mysql_user, mysql_pass, db_name = args
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            mysql_user,
+            mysql_pass,
+            db_name,
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+
+    s = session.add(State(name="Louisiana"))
+    session.commit()
+
+    print(session.query(State).filter(State.name == "Louisiana").one().id)
+    session.close()
