c14803d4feb17c924f0c5e575254f337b339eaeb
Prila1 <yarteypriscy@gmail.com>
2024-01-09 21:23:32 +0000

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Fetches a request to the URL and displays the body of the response (decoded in utf-8)
+Fetches a request to the URL and displays
+ the body of the response (decoded in utf-8)
 """
-
 if __name__ == "__main__":
     import sys
     import requests
