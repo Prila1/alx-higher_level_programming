3717ee17b03caa995939a042396607572088d1e0
Prila1 <yarteypriscy@gmail.com>
2024-01-09 21:21:13 +0000

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Fetches a request to the URL and displays the body of the response (decoded in utf-8)
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+
+    res = requests.get(url)
+
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
