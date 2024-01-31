3828ce34b2a0016bfdfa71adb0c44764430b58cc
Prila1 <yarteypriscy@gmail.com>
2024-01-31 05:53:18 +0000

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const request = require('request');
+
+request.get(argv[2], (_, __, body) => {
+  const ID = 18;
+  console.log(JSON.parse(body).results.filter((film) => {
+    return film.characters.includes(
+      `https://swapi-api.alx-tools.com/api/people/${ID}/`
+    );
+  }).length);
+});
